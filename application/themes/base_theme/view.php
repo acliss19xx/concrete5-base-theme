9a810bc2afe2274e4adf02639b1b0190b0c876c1
Takayuki Kaneko <kaneko@tagworks.jp>
2016-06-29 06:35:22 +0000

    add page template file

--- application/themes/base_theme/view.php
+++ application/themes/base_theme/view.php
@@ -1,18 +1,8 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
-
 <main>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-
-                <?php View::element('system_errors', array('format' => 'block', 'error' => isset($error) ? $error : null, 'success' => isset($success) ? $success : null, 'message' => isset($message) ? $message : null)); ?>
-
-                <?php print $innerContent; ?>
-            </div>
-        </div>
-    </div>
+    <?php View::element('system_errors', array('format' => 'block', 'error' => isset($error) ? $error : null, 'success' => isset($success) ? $success : null, 'message' => isset($message) ? $message : null)); ?>
+    <?php print $innerContent; ?>
 </main>
-
 <?php  $this->inc('elements/footer.php'); ?>
