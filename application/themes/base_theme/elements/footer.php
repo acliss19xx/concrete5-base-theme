ab0e86d200978d5f436eff148a19d630e2fc1f4a
Takayuki Kaneko <kaneko@tagworks.jp>
2016-06-29 06:19:09 +0000

    add page_theme.php comments

--- application/themes/base_theme/elements/footer.php
+++ application/themes/base_theme/elements/footer.php
@@ -1,11 +1,12 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
             <footer>
             <?php
-            $a = new GlobalArea('Footer Site Title');
+            $a = new GlobalArea('Footer Global');
             $a->display();
             ?>
             </footer>
         </div>
+        <?php // ↓必須タグ ?>
         <?php Loader::element('footer_required'); ?>
     </body>
 </html>
